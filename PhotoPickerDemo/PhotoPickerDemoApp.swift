6133dee013d239abfc896cc1448b3199bc818cfe
W1ldW1ngs <88623184+W1ldW1ng@users.noreply.github.com>
2022-04-04 00:42:25 -0500

    Initial Commit

---
+++ PhotoPickerDemo/PhotoPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerDemoApp.swift
+//  PhotoPickerDemo
+//
+//  Created by Eddy Franco on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
